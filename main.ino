2282f2c432be1a69dcf4fcce2e7a8973ac3d1e62
SKParthiv <parthiv.sk.pedapati@gmail.com>
2026-09-21 19:02:32 +0530

    main

---
+++ main.ino
@@ -0,0 +1,16 @@
+// Include Hardware API libraries
+// Custom modules import - a) Auto IMU Calibration
+// module has input from the sensor IMU and gives output of change or delta or maybe new constants values for the PID.
+void setup() {
+  // 1) Pin Definitions
+  // 2) PID constants
+}
+
+void loop() {
+  // 1) Sensor data interpretation
+  // 2) Point velocity calculation
+  // 3) Point velocity interpretation
+  // 4) Opt - PID calibration using the IMU
+  // 5) Then comes the calculation of steering value
+  // 6) Actuation
+}
